93d6ff3be5873154ce6183a8bd5007f94f2d5928
Vinicius Santos <vinicius.santos@bis2bis.com.br>
2023-08-10 08:46:49 -0300

    :hammer: Added css bold style

--- Block/Adminhtml/Column/Renderer/Author.php
+++ Block/Adminhtml/Column/Renderer/Author.php
@@ -6,7 +6,7 @@ class BIS2BIS_Changelog_Block_Adminhtml_Column_Renderer_Author extends Mage_Admi
     {
         $author = $row->getAuthor();
         if(is_array($author)) {
-            $columnData = '<a href="'.$author[1].'" target="_blank">'.ucwords($author[0]).'</a>';
+            $columnData = '<p class="a-center"><a href="'.$author[1].'" target="_blank">'.ucwords($author[0]).'</a></p>';
             return $columnData;
         }
         
