953d217a750e7f62a234b608e488ecb777624552
Vinicius Santos <vinicius.santos@bis2bis.com.br>
2023-08-11 17:14:34 -0300

    :hammer: Css changes

--- Block/Adminhtml/Column/Renderer/Updates.php
+++ Block/Adminhtml/Column/Renderer/Updates.php
@@ -12,29 +12,49 @@ class BIS2BIS_Changelog_Block_Adminhtml_Column_Renderer_Updates extends Mage_Adm
                 $item->getName());
         }
         $rowContent = $row->getData("content/rendered");
-
         if(strlen($rowContent) >= 300) {
             $rowContent = explode(PHP_EOL, strip_tags($rowContent));
-            $rowContent = str_replace(" ", "", $rowContent[0]) == "" ? $rowContent[1] : $rowContent[0];
+            $rowContent = sprintf("<p>%s</p>", str_replace(" ", "", $rowContent[0]) == "" ? $rowContent[1] : $rowContent[0]);
         }
 
-        $showMoreLink = sprintf('<br><a class="changelog-category-link" href="%s" target="_blank">Continue lendo</a>',
+        $showMoreLink = sprintf('<a class="see-more-link" href="%s" target="_blank">Ver mais</a>',
             $row->getLink());
         
         return sprintf(
-            '<div class="changelog-title-container"><a href="%s" target="_blank" class="changelog-title">%s</a></div><div class="changelog-content-container"><div class="changelog-content">%s%s</div></div><div class="changelog-info-container"><div class="changelog-category-container">%s</div><div class="changelog-date-container"><b>%s</b></div></div>',
+            '<div class="changelog-title-container"><a href="%s" target="_blank" class="changelog-title">%s</a></div>
+            <table class="content-table">
+                <tbody>
+                    <tr>
+                        <td><span class="post-info-tag">Categoria </span> </td>
+                        <td><div class="changelog-category-container"><p>%s</p></div></td>
+                    </tr>
+                    <tr>
+                        <td><span class="post-info-tag">Data </span> </td>
+                        <td><div class="changelog-date-container"><p>%s</p></div></td>
+                    </tr>
+                    <tr>
+                        <td><span class="post-info-tag">Conteúdo </span></td>
+                        <td><div class="changelog-content">%s</div></td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">
+                            <div class="changelog-see-more-container">%s</div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>',
             $row->getLink(),
             ucwords($row->getData("title/rendered")),
-            print_r($rowContent, true),
-            $showMoreLink,
             trim($columnData, " / "),
-            $this->treatDate($row->getModified()));
+            $this->treatDate($row->getModified()),
+            $rowContent,
+            $showMoreLink);
     }
 
     public function treatDate($date)
     {
         $timestamp = strtotime($date);
-        $finalDate = date("H:i d/m/Y", $timestamp);
-        return $finalDate;
+        $finalDate = date("d/m/Y H:i", $timestamp);
+        return sprintf("%s", $finalDate);
     }
 }
